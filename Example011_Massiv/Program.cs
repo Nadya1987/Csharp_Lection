5f66821e539e4dd585c059c246d2c6dae8420cc5
Nadya <you@example.com>
2022-06-27 14:24:39 +0300

    Массив из n элеиентов, найти find

---
+++ Example011_Massiv/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 12, 31, 4, 15, 31, 16, 17,18};
+
+int n = array.Length;// возвращает длину или количество элементов массива
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
